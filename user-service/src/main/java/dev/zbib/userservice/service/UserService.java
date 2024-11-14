fbf73d232ae5bd136c5d15c959fd172db32d8676
Mohammad Nasser <mhmdnaser1572@gmail.com>
2024-11-15 03:07:13 +0330

    feat: create service class and add create user method

--- user-service/src/main/java/dev/zbib/userservice/service/UserService.java
+++ user-service/src/main/java/dev/zbib/userservice/service/UserService.java
@@ -1,2 +1,40 @@
-package dev.zbib.userservice.service;public class UserService {
+package dev.zbib.userservice.service;
+
+import dev.zbib.userservice.entity.User;
+import dev.zbib.userservice.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User createUser(Long userId
+            , String firstName
+            , String lastName
+            , String phoneNumber
+            , String password
+            , LocalDate birthDate
+            , String profilePicture
+            , String history
+            , String address) {
+        User user = User
+                .builder()
+                .id(userId)
+                .firstName(firstName)
+                .lastName(lastName)
+                .phoneNumber(phoneNumber)
+                .password(password)
+                .birthDate(birthDate)
+                .profilePicture(profilePicture)
+                .history(history)
+                .address(address)
+                .build();
+        return userRepository.save(user);
+    }
 }
