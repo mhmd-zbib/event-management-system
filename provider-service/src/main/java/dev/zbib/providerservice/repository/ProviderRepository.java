1d43ec5c69ee638587dc056fbb6b727dace58ec6
mhmd-zbib <mohammad.h.zbib@gmail.com>
2024-11-15 00:01:44 +0200

    feat: add provider repository

---
+++ provider-service/src/main/java/dev/zbib/providerservice/repository/ProviderRepository.java
@@ -0,0 +1,10 @@
+package dev.zbib.providerservice.repository;
+
+import dev.zbib.providerservice.model.entity.Provider;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProviderRepository extends JpaRepository<Provider, Long> {
+
+    Provider findByName(String name);
+
+}
