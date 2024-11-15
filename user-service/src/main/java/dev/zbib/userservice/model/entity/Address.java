3eba8fe043c152c4372247671ebcb37a5fa8462d
Mohammad Nasser <mhmdnaser1572@gmail.com>
2024-11-15 18:56:50 +0330

    feat: add embeddible class address from SoC

--- user-service/src/main/java/dev/zbib/userservice/model/entity/Address.java
+++ user-service/src/main/java/dev/zbib/userservice/model/entity/Address.java
@@ -1,2 +1,19 @@
-package dev.zbib.userservice.model.entity;public class Address {
+package dev.zbib.userservice.model.entity;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Embeddable
+public class Address {
+    private String street;
+    private String city;
+    private String postalCode;
+    private String country;
 }
