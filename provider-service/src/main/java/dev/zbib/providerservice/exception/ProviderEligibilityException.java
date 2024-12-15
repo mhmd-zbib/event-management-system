f8cbf6acfccb2569929bd34e378fd4af9f273f18
mhmd-zbib <mohammad.h.zbib@gmail.com>
2024-12-15 10:14:04 +0200

    feat: provider check before creating

--- provider-service/src/main/java/dev/zbib/providerservice/exception/ProviderEligibilityException.java
+++ provider-service/src/main/java/dev/zbib/providerservice/exception/ProviderEligibilityException.java
@@ -9,7 +9,7 @@ public class ProviderEligibilityException extends ProviderException {
     private List<String> reasons;
 
     public ProviderEligibilityException(
-            String reasons) {
+            List<String> reasons) {
         super("You aren't eligible to become a provider for the following reasons:", HttpStatus.FORBIDDEN);
     }
 
--- provider-service/src/main/java/dev/zbib/providerservice/service/ProviderService.java
+++ provider-service/src/main/java/dev/zbib/providerservice/service/ProviderService.java
@@ -17,8 +17,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
-import java.security.InvalidParameterException;
-
 @Service
 @RequiredArgsConstructor
 @Log4j2
@@ -29,26 +27,12 @@ public class ProviderService {
 
     public DetailsResponse createProvider(CreateProviderRequest request) {
         Long id = request.getId();
+        validateProviderEligibility(id);
+        assignProviderRole(id);
 
-        ProviderEligibilityResponse eligibilityResponse = userClient.getProviderEligibility(id);
-        if (!eligibilityResponse.isEligible()) {
-            throw new ProviderEligibilityException(eligibilityResponse.getReasons());
-        }
-
-        try {
-            userClient.setRole(id, UserRole.PROVIDER);
-        } catch (FeignException.NotFound e) {
-            log.error("Error setting provider role", e);
-            throw new ProviderNotFoundException(id);
-        } catch (FeignException.Forbidden e) {
-            log.error("Error setting provider role", e);
-            throw new InvalidParameterException("Provider with id " + id + " is unable to be a provider");
-        } catch (FeignException.BadRequest e) {
-            log.error("Error setting provider role", e);
-        }
         Provider provider = providerMapper.toProvider(request);
-        Provider createdUser = providerRepository.save(provider);
-        return providerMapper.toDetailsResponse(createdUser);
+        Provider createdProvider = providerRepository.save(provider);
+        return providerMapper.toDetailsResponse(createdProvider);
     }
 
     public ProviderResponse getProviderById(Long id) {
@@ -63,4 +47,26 @@ public class ProviderService {
         }
         return providerMapper.toProviderResponse(user, details);
     }
+
+    private void validateProviderEligibility(Long id) {
+        try {
+            ProviderEligibilityResponse eligibilityResponse = userClient.getProviderEligibility(id);
+            if (!eligibilityResponse.isEligible()) {
+                throw new ProviderEligibilityException(eligibilityResponse.getReasons());
+            }
+        } catch (FeignException.NotFound e) {
+            log.error("Provider with id {} not found", id);
+            throw new ProviderNotFoundException(id);
+        }
+    }
+
+    private void assignProviderRole(Long id) {
+        try {
+            userClient.setRole(id, UserRole.PROVIDER);
+        } catch (FeignException.NotFound e) {
+            log.error("Provider with id {} not found", id);
+            throw new ProviderNotFoundException(id);
+        }
+    }
+
 }
