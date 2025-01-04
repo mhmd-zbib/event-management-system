a0614c3d01a58a31dd574dfc3c100f89104d7b47
mhmd-zbib <mohammad.h.zbib@gmail.com>
2025-01-04 22:26:25 +0200

    feature: refactor provider

--- provider-service/src/main/java/dev/zbib/providerservice/dto/internal/ProviderValidationDTO.java
+++
@@ -1,9 +0,0 @@
-package dev.zbib.providerservice.dto.internal;
-
-import dev.zbib.shared.enums.ServiceType;
-
-public record ProviderValidationDTO(
-        ServiceType serviceType,
-        boolean available
-) {
-}
--- provider-service/src/main/java/dev/zbib/providerservice/dto/response/UserListResponse.java
+++
@@ -1,9 +0,0 @@
-package dev.zbib.providerservice.dto.response;
-
-public record UserListResponse(
-        Long id,
-        String firstName,
-        String lastName,
-        String profilePicture
-) {
-}
