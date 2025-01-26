bc3cd7ffb32305222d43ff28258b223a6cf3f922
mhmd-zbib <mohammad.h.zbib@gmail.com>
2025-01-26 20:01:28 +0200

    feat: add image entity

--- venue-service/src/main/java/dev/zbib/venueservice/dto/VenueCreationRequest.java
+++ venue-service/src/main/java/dev/zbib/venueservice/dto/VenueCreationRequest.java
@@ -3,6 +3,8 @@ package dev.zbib.venueservice.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.UUID;
+
 @Getter
 @Setter
 public class VenueCreationRequest {
@@ -10,4 +12,15 @@ public class VenueCreationRequest {
     private String description;
     private int maxCapacity;
     private int bookingTimeline;
+
+    @Getter
+    @Setter
+    private static class ImageRequest {
+        private UUID entityId;
+        private String url;
+        private int sort_order;
+        private int size;
+        private int width;
+        private int height;
+    }
 }
--- venue-service/src/main/java/dev/zbib/venueservice/entity/Image.java
+++ venue-service/src/main/java/dev/zbib/venueservice/entity/Image.java
@@ -1,16 +1,37 @@
 package dev.zbib.venueservice.entity;
 
+import dev.zbib.venueservice.enums.EntityType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Image {
 
     @Id
     private UUID id;
 
+    private UUID entityId;
 
+    private EntityType entityType;
+
+    private String url;
+
+    private int sort_order;
+
+    private int size;
+
+    private int width;
+
+    private int height;
 
 }
