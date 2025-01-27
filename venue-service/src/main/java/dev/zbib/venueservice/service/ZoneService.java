2460327e31f533ef7525b115f748491bb6af4501
mhmd-zbib <mohammad.h.zbib@gmail.com>
2025-01-27 08:29:27 +0200

    feat: implement get venue by id

--- venue-service/src/main/java/dev/zbib/venueservice/service/ZoneService.java
+++ venue-service/src/main/java/dev/zbib/venueservice/service/ZoneService.java
@@ -2,22 +2,28 @@ package dev.zbib.venueservice.service;
 
 import dev.zbib.venueservice.dto.ZoneCreationRequest;
 import dev.zbib.venueservice.dto.ZoneCreationResponse;
+import dev.zbib.venueservice.entity.Venue;
 import dev.zbib.venueservice.entity.Zone;
 import dev.zbib.venueservice.repository.ZoneRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class ZoneService {
 
     private final ZoneRepository zoneRepository;
+    private final VenueService venueService;
 
-    public ZoneCreationResponse createZone(ZoneCreationRequest request) {
+    public ZoneCreationResponse createZone(UUID venueId, ZoneCreationRequest request) {
+        Venue venue = venueService.getVenueById(venueId);
         Zone zone = Zone
                 .builder()
                 .name(request.getName())
                 .description(request.getDescription())
+                .venue(venue)
                 .build();
         Zone savedZone = zoneRepository.save(zone);
         return ZoneCreationResponse
